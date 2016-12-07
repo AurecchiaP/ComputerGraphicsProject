59ea3375d2fd2d5ab2bab63a30ecc92f24df6eae
Samuele Decarli <samuele.decarli@usi.ch>
2016-12-07 10:01:45 +0100

    Store transformation matrices in TrackPieceType, instead of function

--- project_base_code/TrackPieceType.cpp
+++ project_base_code/TrackPieceType.cpp
@@ -1,17 +1,27 @@
 #include "TrackPieceType.h"
 
-TrackPieceType::TrackPieceType
-(const std::string & filename, std::function<void()> transform) :
-    model(filename), transform(transform) {}
+TrackPieceType::TrackPieceType (const std::string & filename, GLdouble * src_mat) : model(filename) {
+    memcpy(mat, src_mat, sizeof(GLdouble) * 16);
+}
+
+TrackPieceType::TrackPieceType (const std::string & filename) : model(filename) {}
 
 void TrackPieceType::draw() {
     model.draw();
 }
 
 void TrackPieceType::applyTransforms() {
-    transform();
+    glMultMatrixd(mat);
 }
 
 void TrackPieceType::init() {
     model.init();
+}
+
+void TrackPieceType::copyMatrix(GLdouble * src_matrix) {
+    memcpy(mat, src_matrix, sizeof(GLdouble) * 16);
+}
+
+GLdouble * TrackPieceType::getMatrix() {
+    return mat;
 }
--- project_base_code/TrackPieceType.h
+++ project_base_code/TrackPieceType.h
@@ -9,12 +9,15 @@ class TrackPieceType
 {
 private:
     ObjModel model;
-    const std::function<void()> transform;
+    GLdouble mat[16];
 public:
-    TrackPieceType(const std::string & filename, std::function<void()> transform);
+    TrackPieceType(const std::string & filename, GLdouble * src_mat);
+    TrackPieceType(const std::string & filename);
     void draw();
     void applyTransforms();
     void init();
+    void copyMatrix(GLdouble * src_mat);
+    GLdouble * getMatrix();
 };
 
 #endif // TRACKPIECETYPE_H
