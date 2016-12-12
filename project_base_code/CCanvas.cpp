4c6725cff7c3199d7f23eeadeb75c232799f28b9
Samuele Decarli <samuele.decarli@usi.ch>
2016-12-12 14:25:39 +0100

    Cockpit view

--- project_base_code/CCanvas.cpp
+++ project_base_code/CCanvas.cpp
@@ -341,7 +341,160 @@ void CCanvas::setView(View _view) {
         glRotated(-30, 1.0, 0.0, 0.0);
         break;
     case Cockpit:
-        // Maybe you want to have an option to view the scene from the train cockpit, up to you
+        GLdouble m[16];
+        // Compute matrix at train position
+        glPushMatrix();
+        glScaled(0.2, 0.2, 0.2);
+        size_t i = 0;
+        double currentPosition = trainPosition;
+        for (TrainPieceType * piece : train) {
+            currentPosition += piece->len;
+        }
+        while (currentPosition >= track[i]->len) {
+            currentPosition -= track[i]->len;
+            track[i]->applyTransforms();
+            i = (i + 1) % track.size();
+        }
+
+        track[i]->applyTransforms(currentPosition / track[i]->len);
+
+        glTranslated(0, 3.99761/2.0, 3 + 0.922535);
+        glRotated(90, 0, 0, 1);
+        glRotated(90, 1, 0, 0);
+
+        glGetDoublev(GL_MODELVIEW, m);
+
+        glPopMatrix();
+
+
+        // Invert matrix
+        double inv[16];
+        double det;
+
+        inv[0] = m[5]  * m[10] * m[15] -
+                 m[5]  * m[11] * m[14] -
+                 m[9]  * m[6]  * m[15] +
+                 m[9]  * m[7]  * m[14] +
+                 m[13] * m[6]  * m[11] -
+                 m[13] * m[7]  * m[10];
+
+        inv[4] = -m[4]  * m[10] * m[15] +
+                  m[4]  * m[11] * m[14] +
+                  m[8]  * m[6]  * m[15] -
+                  m[8]  * m[7]  * m[14] -
+                  m[12] * m[6]  * m[11] +
+                  m[12] * m[7]  * m[10];
+
+        inv[8] = m[4]  * m[9] * m[15] -
+                 m[4]  * m[11] * m[13] -
+                 m[8]  * m[5] * m[15] +
+                 m[8]  * m[7] * m[13] +
+                 m[12] * m[5] * m[11] -
+                 m[12] * m[7] * m[9];
+
+        inv[12] = -m[4]  * m[9] * m[14] +
+                   m[4]  * m[10] * m[13] +
+                   m[8]  * m[5] * m[14] -
+                   m[8]  * m[6] * m[13] -
+                   m[12] * m[5] * m[10] +
+                   m[12] * m[6] * m[9];
+
+        inv[1] = -m[1]  * m[10] * m[15] +
+                  m[1]  * m[11] * m[14] +
+                  m[9]  * m[2] * m[15] -
+                  m[9]  * m[3] * m[14] -
+                  m[13] * m[2] * m[11] +
+                  m[13] * m[3] * m[10];
+
+        inv[5] = m[0]  * m[10] * m[15] -
+                 m[0]  * m[11] * m[14] -
+                 m[8]  * m[2] * m[15] +
+                 m[8]  * m[3] * m[14] +
+                 m[12] * m[2] * m[11] -
+                 m[12] * m[3] * m[10];
+
+        inv[9] = -m[0]  * m[9] * m[15] +
+                  m[0]  * m[11] * m[13] +
+                  m[8]  * m[1] * m[15] -
+                  m[8]  * m[3] * m[13] -
+                  m[12] * m[1] * m[11] +
+                  m[12] * m[3] * m[9];
+
+        inv[13] = m[0]  * m[9] * m[14] -
+                  m[0]  * m[10] * m[13] -
+                  m[8]  * m[1] * m[14] +
+                  m[8]  * m[2] * m[13] +
+                  m[12] * m[1] * m[10] -
+                  m[12] * m[2] * m[9];
+
+        inv[2] = m[1]  * m[6] * m[15] -
+                 m[1]  * m[7] * m[14] -
+                 m[5]  * m[2] * m[15] +
+                 m[5]  * m[3] * m[14] +
+                 m[13] * m[2] * m[7] -
+                 m[13] * m[3] * m[6];
+
+        inv[6] = -m[0]  * m[6] * m[15] +
+                  m[0]  * m[7] * m[14] +
+                  m[4]  * m[2] * m[15] -
+                  m[4]  * m[3] * m[14] -
+                  m[12] * m[2] * m[7] +
+                  m[12] * m[3] * m[6];
+
+        inv[10] = m[0]  * m[5] * m[15] -
+                  m[0]  * m[7] * m[13] -
+                  m[4]  * m[1] * m[15] +
+                  m[4]  * m[3] * m[13] +
+                  m[12] * m[1] * m[7] -
+                  m[12] * m[3] * m[5];
+
+        inv[14] = -m[0]  * m[5] * m[14] +
+                   m[0]  * m[6] * m[13] +
+                   m[4]  * m[1] * m[14] -
+                   m[4]  * m[2] * m[13] -
+                   m[12] * m[1] * m[6] +
+                   m[12] * m[2] * m[5];
+
+        inv[3] = -m[1] * m[6] * m[11] +
+                  m[1] * m[7] * m[10] +
+                  m[5] * m[2] * m[11] -
+                  m[5] * m[3] * m[10] -
+                  m[9] * m[2] * m[7] +
+                  m[9] * m[3] * m[6];
+
+        inv[7] = m[0] * m[6] * m[11] -
+                 m[0] * m[7] * m[10] -
+                 m[4] * m[2] * m[11] +
+                 m[4] * m[3] * m[10] +
+                 m[8] * m[2] * m[7] -
+                 m[8] * m[3] * m[6];
+
+        inv[11] = -m[0] * m[5] * m[11] +
+                   m[0] * m[7] * m[9] +
+                   m[4] * m[1] * m[11] -
+                   m[4] * m[3] * m[9] -
+                   m[8] * m[1] * m[7] +
+                   m[8] * m[3] * m[5];
+
+        inv[15] = m[0] * m[5] * m[10] -
+                  m[0] * m[6] * m[9] -
+                  m[4] * m[1] * m[10] +
+                  m[4] * m[2] * m[9] +
+                  m[8] * m[1] * m[6] -
+                  m[8] * m[2] * m[5];
+
+        det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+        if (det == 0)
+            break;
+
+        det = 1.0 / det;
+
+        for (i = 0; i < 16; i++)
+            inv[i] = inv[i] * det;
+
+        // Use inverted matrix
+        glMultMatrixd(inv);
         break;
     }
 }
@@ -358,7 +511,7 @@ void CCanvas::paintGL()
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     // Setup the current view
-    setView(View::Perspective);
+    setView(View::Cockpit);
 
     // You can always change the light position here if you want
     GLfloat lightpos[] = {-4.0f, 1.0f, 20.0f, 1.0f};
